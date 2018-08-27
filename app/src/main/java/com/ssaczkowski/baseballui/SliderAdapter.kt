a2525537e0eca2bda20658b67fb404b093783504
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2018-08-27 14:29:11 -0300

    Update

--- app/src/main/java/com/ssaczkowski/baseballui/SliderAdapter.kt
+++ app/src/main/java/com/ssaczkowski/baseballui/SliderAdapter.kt
@@ -2,6 +2,7 @@ package com.ssaczkowski.baseballui
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Typeface
 import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
@@ -18,10 +19,12 @@ class SliderAdapter(var context: Context) : PagerAdapter() {
         layoutInfalter = LayoutInflater.from(context)
     }
 
-    var slide_background: IntArray = intArrayOf(R.drawable.gradientintro2, R.drawable.gradient, R.drawable.gradientintro3)
-    var slide_images: IntArray = intArrayOf(R.drawable.ic_baseball_intro1, R.drawable.ic_baseball_intro2, R.drawable.ic_baseball_intro3)
-    var slide_headings = arrayOf(R.string.welcome, R.string.slide_intro_enjoy, R.string.slide_intro_social)
+
+    var slide_background: IntArray = intArrayOf(R.drawable.gradientintro2, R.drawable.gradient,R.drawable.gradientintro3)
+    var slide_images: IntArray = intArrayOf(R.drawable.ic_baseball_intro1, R.drawable.ic_baseball_intro2,R.drawable.ic_baseball_intro3)
+    var slide_headings = arrayOf(R.string.welcome, R.string.slide_intro_enjoy,R.string.slide_intro_social)
     var slide_description = arrayOf(R.string.slide_intro_welcome_description, R.string.slide_intro_enjoy_description, R.string.slide_intro_social_description)
+
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view === `object` as (RelativeLayout)
@@ -39,20 +42,23 @@ class SliderAdapter(var context: Context) : PagerAdapter() {
         view.slide_textView_tittle.setText(slide_headings[position])
         view.slide_textView_description.setText(slide_description[position])
 
-       /* if (position == 1) {
+        if (position == 0) {
             view.slide_textView_point1.setTextColor(context.resources.getColor(R.color.colorWhite))
+            view.slide_textView_point1.setTypeface(null, Typeface.BOLD)
             view.slide_textView_point1.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
             view.slide_textView_point1.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
-        } else if (position == 2) {
+        } else if (position == 1) {
             view.slide_textView_point2.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
             view.slide_textView_point2.setTextColor(context.resources.getColor(R.color.colorWhite))
+            view.slide_textView_point2.setTypeface(null, Typeface.BOLD)
             view.slide_textView_point2.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
-        } else if (position == 3) {
+        } else if (position == 2) {
             view.slide_textView_point3.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
             view.slide_textView_point3.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
             view.slide_textView_point3.setTextColor(context.resources.getColor(R.color.colorWhite))
+            view.slide_textView_point3.setTypeface(null, Typeface.BOLD)
         }
-*/
+
         container.addView(view)
 
         return view
