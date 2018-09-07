7e95bf6777f300f72c88723dbd6467e4c2553ee8
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2018-09-07 17:35:45 -0300

    Update

--- app/src/main/java/com/ssaczkowski/baseballui/StartActivity.kt
+++ app/src/main/java/com/ssaczkowski/baseballui/StartActivity.kt
@@ -2,11 +2,24 @@ package com.ssaczkowski.baseballui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
+
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+       // toggle = ActionBarDrawerToggle(this, startActivityDrawerLayout, R.string.open,R.string.close)
+        //startActivityDrawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
     }
 }
