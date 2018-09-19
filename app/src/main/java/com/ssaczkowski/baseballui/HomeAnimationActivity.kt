6a60e1da2cc17bdc73fd600d6e32f3ad18e6048a
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2018-09-19 10:10:10 -0300

    Add transition with positions

--- app/src/main/java/com/ssaczkowski/baseballui/HomeAnimationActivity.kt
+++ app/src/main/java/com/ssaczkowski/baseballui/HomeAnimationActivity.kt
@@ -1,9 +1,12 @@
 package com.ssaczkowski.baseballui
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home_animation.*
+import android.util.Pair
+import android.view.View
 
 class HomeAnimationActivity : AppCompatActivity() {
 
@@ -18,6 +21,20 @@ class HomeAnimationActivity : AppCompatActivity() {
         home_animation_text_view_fadeinout.setOnClickListener {
             startActivity(Intent(this@HomeAnimationActivity, FadeActivity::class.java))
             overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+        }
+
+        home_animation_text_view_transition.setOnClickListener {
+
+            var pair : Pair<View,String> =  Pair<View,String> (activity_home_animation_image_view,"image_view_transition")
+
+            var pair2 : Pair<View,String>  = Pair<View,String> (home_animation_text_view_transition,"text_view_transition")
+
+            var intent = Intent(this@HomeAnimationActivity,TransitionActivity::class.java)
+
+            var options = ActivityOptions.makeSceneTransitionAnimation(this,pair,pair2)
+
+            startActivity(intent,options.toBundle())
+
         }
 
     }
